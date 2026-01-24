efa887f4fd18379d972696a541efcddca5b0652c
Miguel Angel <miguelangelgarb@gmail.com>
2026-01-24 10:00:43 -0500

    Created view home fragment

--- app/src/main/java/com/mgarzondev/airquality/ui/main/MainActivity.kt
+++ app/src/main/java/com/mgarzondev/airquality/ui/main/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, 0, systemBars.right,0)
             insets
         }
 
